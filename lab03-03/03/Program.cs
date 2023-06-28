ff92ca8192def3909d9ee7371290cbc108822a32
PKS Amasha <lukasu713@gmail.com>
2023-06-28 12:13:01 +0530

    Add files via upload

---
+++ lab03-03/03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 2, 8, 3, 1 };
+
+            // Sort in ascending order using bubble sort
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // Swap elements
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Ascending order:");
+            Console.WriteLine(string.Join(" ", array));
+
+            // Sort in descending order using bubble sort
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        // Swap elements
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Descending order:");
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
