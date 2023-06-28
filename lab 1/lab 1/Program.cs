31d58d7f696286d4f995f825f7233936b013ad82
PKS Amasha <lukasu713@gmail.com>
2023-06-28 11:40:35 +0530

    Add files via upload

---
+++ lab 1/lab 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("sakuni 20 Yakkala" );
+           
+
+
+            
+        }
+    }
+}
