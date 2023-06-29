5d93922f6931cf80ad8af7284e8a5bb8d1cf1938
PKS Amasha <lukasu713@gmail.com>
2023-06-29 11:24:54 +0530

    Add files via upload

---
+++ lab04/lab04/FindValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+    internal class FindValue
+    {
+        public double findArea(double r)
+        {
+            double area = r * r * 22 / 7;
+            return area;
+        }
+        public double findCircumference(double r)
+        {
+            double circumference = 2 * r * 22 / 7;
+            return circumference;
+        }
+
+    }
+}
---
+++ lab04/lab04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("enter the radius:");
+                double radius = double.Parse(Console.ReadLine());
+
+                FindValue findValue = new FindValue();
+                double area = findValue.findArea(radius);
+                Console.WriteLine("Area is:" + area);
+
+                double circumference = findValue.findCircumference(radius);
+                Console.WriteLine("Circumference is:" + circumference);
+            
+            
+                Console.ReadKey();
+        }
+    }
+}
