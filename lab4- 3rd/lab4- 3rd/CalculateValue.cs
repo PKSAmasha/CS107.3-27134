8bbefa6a5cd6a7074da974167f0cca717fa481e3
PKS Amasha <lukasu713@gmail.com>
2023-06-29 11:25:45 +0530

    Add files via upload

---
+++ lab4- 3rd/lab4- 3rd/CalculateValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4__3rd
+{
+    internal class CalculateValue
+    {
+        public double
+           Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public double
+             Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double
+            Multi(double a, double b)
+        {
+            return a * b;
+        }
+        public double
+            div(double a, double b)
+        {
+            if (a != 0)
+                return a / b;
+            else
+            {
+                Console.WriteLine("cannot divide by zero");
+                return 0;
+            }
+        }
+    }
+}
---
+++ lab4- 3rd/lab4- 3rd/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4__3rd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("methods;");
+            Console.WriteLine("enter 1 for addition");
+            Console.WriteLine("enter 2 for addition");
+            Console.WriteLine("enter 3 for addition");
+            Console.WriteLine("enter 4 for addition");
+
+            Console.WriteLine("enter your choice:");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("enter a value for a :");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter a value for b :");
+            double b = double.Parse(Console.ReadLine());
+
+            CalculateValue obj = new CalculateValue();
+            double answer = 0;
+
+            if (input == "1")
+            {
+                answer = obj.Sum(a, b);
+            }
+            else if (input == "2")
+            {
+                answer = obj.Sub(a, b);
+            }
+            else if (input == "3")
+            {
+                answer = obj.Multi(a, b);
+            }
+            else if (input == "4")
+            {
+                answer = obj.div(a, b);
+            }
+            Console.WriteLine("Answer is:" + answer);
+            Console.ReadLine();
+        }
+    }
+}
