e92e175bdd2f7dfae211f9578e7235842f4519a8
PKS Amasha <lukasu713@gmail.com>
2023-07-04 10:55:13 +0530

    Add files via upload

---
+++ lab05-Q6/lab05-Q6/Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab05_Q6
+{
+    internal class Array
+    {
+        public int[] ArrayMethod(int size)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                Console.WriteLine("Enter values for the array:");
+                int value = Convert.ToInt32(Console.ReadLine());
+                array[i] = value;
+                array[i + 1] = 0;
+            }
+
+            return array;
+
+        }
+    }
+}
+
+
---
+++ lab05-Q6/lab05-Q6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab05_Q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Array obj = new Array();
+            int[] array = obj.ArrayMethod(size);
+
+            Console.WriteLine("\n Display the array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + " ");
+
+            }
+                Console.ReadLine();
+        }
+    }
+}
