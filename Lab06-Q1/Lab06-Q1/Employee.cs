a17018eb2aa2254be1402ae3937188828a1310be
PKS Amasha <lukasu713@gmail.com>
2023-07-11 10:24:03 +0530

    Add files via upload

---
+++ Lab06-Q1/Lab06-Q1/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_Q1
+{
+    internal class Employee
+    {
+        private int empID;
+        private string empName;
+        private string empDesignation;
+
+      //setter
+        public void  setempID(int input)
+        {
+            empID = input;
+        }
+
+        public void setempName(string input)
+        {
+            empName = input;
+        }
+        public void setempDesignation(string input)
+        {
+            empDesignation = input;
+        }
+        //getter(read variable)
+        public int getempID()
+        {
+            return empID;
+        }
+        public string getempName()
+        {
+            return empName;
+        }
+        public string getempDesignation()
+        {
+            return empDesignation;
+        }
+    }
+}
+
---
+++ Lab06-Q1/Lab06-Q1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_Q1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Enter Employee ID: ");
+            int empID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Employee Name: ");
+            string empName = Console.ReadLine();
+
+            Console.Write("Enter Employee Designation: ");
+            string empDesignation = Console.ReadLine();
+
+            Employee empObj = new Employee();
+
+            empObj.setempID(empID);
+            empObj.setempName(empName);
+            empObj.setempDesignation(empDesignation);
+
+               
+
+                
+                Console.WriteLine($"Hello" +empObj.getempName()+ "Your Employee ID is"+empObj.getempID()+"  and You are a " +empObj.getempDesignation());
+            }
+        }
+    }
+
