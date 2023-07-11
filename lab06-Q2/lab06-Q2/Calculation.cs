22ffca778d8cbd413a40857decdbc133ac72c623
PKS Amasha <lukasu713@gmail.com>
2023-07-11 11:06:12 +0530

    Add files via upload

---
+++ lab06-Q2/lab06-Q2/Calculation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06_Q2
+{
+    internal class Calculation
+    {
+        private double number1;
+        private double number2;
+
+        private void setValues(double num1, double num2)
+        { 
+            number1= num1;
+            number2= num2;
+        }
+
+        public double getAdd()
+        { 
+            return number1 + number2;
+        }
+        public double getSub()
+        {
+            return number1 - number2; 
+        }
+        public double getMulti()
+        {
+            return number1 * number2; 
+        }
+        public double getdiv()
+        {
+            return (number1 / number2);
+        }
+    }
+}
---
+++ lab06-Q2/lab06-Q2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value for number 1:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value for number 2:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Calculation calObj = new Calculation();
+
+            calObj.setValues(num1, num2);
+
+            Console.WriteLine("Sum is: " + calObj.getAdd());
+            Console.WriteLine("Sub is: " + calObj.getSub());
+            Console.WriteLine("Multi is: " + calObj.getMulti());
+            Console.WriteLine("Div is: " + calObj.getdiv());
+
+        }
+    }
+}
